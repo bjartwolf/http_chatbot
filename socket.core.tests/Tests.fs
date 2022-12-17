8c62bf1a15c21a6554e9ba1b3dd5b331d304442d
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2022-12-17 21:06:32 +0100

    Initial idea for some wrappers

---
+++ socket.core.tests/Tests.fs
@@ -0,0 +1,9 @@
+module Tests
+
+open System
+open Xunit
+open socket.core.TcpWrappers
+
+[<Fact>]
+let ``My test`` () =
+    Assert.True(true)
---
+++ socket.core/TcpClientWrapper.fs
@@ -0,0 +1,38 @@
+﻿namespace socket.core
+
+module TcpWrappers =
+    open System
+    open System.Threading
+    open System.Net
+    open System.Net.Sockets
+
+    type ITcpClient =
+       abstract member GetStream: unit -> IO.Stream 
+       abstract member Close: unit -> unit 
+
+    type TcpClientWrapper (innerClient: TcpClient) = 
+       let mutable _innerClient = innerClient
+
+       interface ITcpClient with
+           member this.Close(): unit = 
+               _innerClient.Close() 
+           member this.GetStream(): IO.Stream = 
+               _innerClient.GetStream() 
+
+    type ITcpListener = 
+       abstract member Start: unit -> unit
+       abstract member AcceptTcpClientAsync: CancellationToken -> Async<ITcpClient> 
+
+    type TcpListenerWrapper (tcpListener: TcpListener) =
+       let mutable _innerTcpLisneter = tcpListener
+       interface ITcpListener with
+           member this.AcceptTcpClientAsync(ct: CancellationToken): Async<ITcpClient> = 
+                  async {
+                    let! tcpClient = _innerTcpLisneter.AcceptTcpClientAsync(ct).AsTask() |> Async.AwaitTask 
+                    return new TcpClientWrapper(tcpClient) 
+                  }
+
+           member this.Start(): unit = 
+               raise (System.NotImplementedException())
+
+       
