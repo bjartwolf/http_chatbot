b617362ada034fea6b6cb5b402ad12b51ea97f3a
bjorn-einar-bjartnes-4ss <bjartnes@gmail.com>
2023-01-21 11:28:48 +0100

    Trying to see how it all can be wired up

--- socket.terminal.elmish/Program.fs
+++ socket.terminal.elmish/Program.fs
@@ -1,31 +1,54 @@
 ﻿module Counter
 
 open Terminal.Gui.Elmish
+open System.Net
+open Terminal.Gui
+open ConnectionController
+open System
 
+// The model with the server object, not very immutable is it... 
 type Model = {
-    IsSpinning: bool
+    Connections: IPEndPoint list 
+    Server: MailboxProcessor<ConnectionController.ConnectionMsg>
 }
 
 type Msg =
-    | Increment
+    | ConnectionEstablished of IPEndPoint 
+    | Tick 
 
 let init () : Model * Cmd<Msg> =
     let model = {
-        IsSpinning = false
+        Connections = [] 
+        Server = Server.server 
     }
     model, Cmd.none
 
-
-
 let update (msg:Msg) (model:Model) =
-    model, Cmd.none
+    let reply = model.Server.PostAndReply(fun channel -> (GetNewConnection channel))
+    match reply with 
+        | Some (client, iPEndPoint) -> { model with Connections = iPEndPoint :: model.Connections } , Cmd.none
+        | None -> model, Cmd.none 
 
 let view (model:Model) (dispatch:Msg->unit) =
     View.page [
         page.menuBar [
         ]
+        prop.children [
+            View.label [
+                prop.position.x.center
+                prop.position.y.at 1
+                prop.textAlignment.left
+                prop.color (Color.BrightYellow, Color.Green)
+                label.text $"hei {model.Connections.Length}" 
+            ] 
+            View.button [
+                prop.position.x.center
+                prop.position.y.at 4
+                label.text "Refresh"
+                prop.onKeyDown (fun _ -> dispatch (Tick))
+            ]
+        ] 
     ]
-
 
 Program.mkProgram init update view  
 |> Program.run
---
+++ socket.terminal.elmish/Server.fs
@@ -0,0 +1,16 @@
+﻿module Server
+
+open System.Threading
+open System.Net
+open socket.core.TcpWrappers
+open ConnectionController
+open System.Net.Sockets
+
+let private cts = new CancellationTokenSource()
+let private port = 13001;
+let private localAddr = IPAddress.Parse("127.0.0.1");
+let private ep = new IPEndPoint(localAddr, port);
+
+let private tcpListener : ITcpListener = new TcpListenerWrapper(new TcpListener(ep)) 
+
+let server = ListenConnections(tcpListener, cts.Token)
