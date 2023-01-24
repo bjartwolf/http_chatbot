b0b14fb142bff2158c1f28bf8e243e9132805243
Bjorn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-01-24 19:24:45 +0100

    refactor views

--- socket.terminal.elmish/Program.fs
+++ socket.terminal.elmish/Program.fs
@@ -88,6 +88,34 @@ let getSelectedItem (connections: Connection list) (selectedConnection: Connecti
         | Some (_,selected)-> (connections|> List.findIndex ( fun (_,c) -> c = selected))
     foo
 
+let contentView (position: int) (title: string) (content: string) = 
+    View.frameView [
+        frameView.title title 
+        prop.position.x.at 0
+        prop.position.y.percent position 
+        prop.height.percent 40.0
+        prop.width.filled
+        frameView.children [
+            View.scrollView [
+                prop.position.x.at 0
+                prop.position.y.at 0
+                prop.height.filled
+                prop.width.filled
+                scrollView.contentSize (Size(80,80))
+                scrollView.children [
+                    View.textView [
+                        prop.position.x.percent 0
+                        prop.position.y.percent 0
+                        prop.width.filled
+                        prop.height.filled
+                        textView.readOnly true 
+                        textField.text content
+                    ]
+                ]
+            ]
+        ]
+    ]
+
 let view (model:Model) (dispatch:Msg->unit) =
     View.page [
         View.window [
@@ -123,7 +151,7 @@ let view (model:Model) (dispatch:Msg->unit) =
                 ]
                 View.frameView [
                     let title = match model.SelectedItem with
-                        | Some (_,conn) -> sprintf "%A:%A" conn.Address conn.Port
+                        | Some (_,conn) -> sprintf "%A:%A" conn.Address conn.Port 
                         | None -> "No connection selected"
                     frameView.title title 
                     prop.position.x.at 20
@@ -131,30 +159,8 @@ let view (model:Model) (dispatch:Msg->unit) =
                     prop.width.percent 80.0
                     prop.height.filled
                     frameView.children [
-                            View.scrollView [
-                                prop.position.x.at 0
-                                prop.position.y.at 0
-                                prop.height.fill 10
-                                prop.width.filled
-                                scrollView.contentSize (Size(80,80))
-                                scrollView.children [
-                                View.textView [
-                                    prop.position.x.percent 0
-                                    prop.position.y.percent 0
-                                    prop.width.filled
-                                    prop.height.filled
-                                    textView.readOnly true 
-                                    textField.text model.SelectedConnectionRecieved ]
-                                ]
-                            ]
-
-                            View.textView [
-                                prop.position.x.at 0
-                                prop.position.y.percent 50.0
-                                prop.width.filled
-                                prop.height.filled
-                                textView.readOnly true 
-                                textField.text model.SelectedConnectionSent ]
+                            contentView 0 "Client" model.SelectedConnectionRecieved
+                            contentView 50 "Server" model.SelectedConnectionSent
                             View.textField [
                                 prop.position.x.at 0
                                 prop.position.y.percent 90.0
@@ -171,7 +177,7 @@ let view (model:Model) (dispatch:Msg->unit) =
                             ]
                     ]
                 ]
-           ] 
+            ] 
         ]
     ]
 
