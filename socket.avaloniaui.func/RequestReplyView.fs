e4ff8603983f346080f936118f29a11f8ec7d0d0
bjorn-einar-bjartnes-4ss <bjartnes@gmail.com>
2023-10-01 12:48:07 +0200

    Move request response view into module

---
+++ socket.avaloniaui.func/RequestReplyView.fs
@@ -0,0 +1,47 @@
+﻿module RequestReplyView
+
+open Avalonia.FuncUI.DSL
+open Avalonia.Controls
+open Avalonia.Media
+open Avalonia.Controls.Primitives
+
+let view (selectedContextRecieved: string) (selectedContentSent: string) =
+    Grid.create [
+        Grid.rowDefinitions "50*,1*,50*" 
+        Grid.children[
+            Border.create [
+                Border.borderThickness 1
+                Border.borderBrush Brushes.Gray
+                Border.margin 2
+                Border.child (
+                    ScrollViewer.create [
+                        ScrollViewer.horizontalScrollBarVisibility ScrollBarVisibility.Visible
+                        ScrollViewer.verticalScrollBarVisibility ScrollBarVisibility.Visible
+                        Grid.row 0
+                        ScrollViewer.content (
+                             TextBlock.create [
+                                TextBlock.horizontalScrollBarVisibility ScrollBarVisibility.Disabled
+                                TextBlock.verticalScrollBarVisibility ScrollBarVisibility.Disabled
+                                TextBlock.text selectedContextRecieved 
+                            ]
+                        )
+                    ]
+                 )
+            ]
+            GridSplitter.create [
+                Grid.row 1
+            ]
+            ScrollViewer.create [
+                ScrollViewer.horizontalScrollBarVisibility ScrollBarVisibility.Visible
+                ScrollViewer.verticalScrollBarVisibility ScrollBarVisibility.Visible
+                Grid.row 2
+                ScrollViewer.content (
+                    TextBlock.create [
+                        TextBlock.horizontalScrollBarVisibility ScrollBarVisibility.Disabled
+                        TextBlock.verticalScrollBarVisibility ScrollBarVisibility.Disabled
+                        TextBlock.text selectedContentSent 
+                    ]
+                )
+            ]
+        ]
+    ]
--- socket.avaloniaui.func/Views.fs
+++ socket.avaloniaui.func/Views.fs
@@ -1,8 +1,5 @@
 ﻿namespace Socket.AvaloniaUi 
 
-open Avalonia.Controls.Primitives
-open Avalonia.Media
-
 module SocketViews = 
     open Avalonia.Controls
     open Model
@@ -14,48 +11,6 @@ module SocketViews =
             DockPanel.children [
                 ControlsView.view model dispatch
                 ConnectionView.view model.Connections dispatch
-
-                Grid.create [
-                    Grid.rowDefinitions "50*,1*,50*" 
-                    Grid.children[
-                        Border.create [
-                            Border.borderThickness 1
-                            Border.borderBrush Brushes.Gray
-                            Border.margin 2
-                            Border.child (
-                                ScrollViewer.create [
-                                    //ScrollViewer.borderBrush Brushes.Black
-                                    //ScrollViewer.borderThickness 2
-                                    ScrollViewer.horizontalScrollBarVisibility ScrollBarVisibility.Visible
-                                    ScrollViewer.verticalScrollBarVisibility ScrollBarVisibility.Visible
-                                    Grid.row 0
-                                    ScrollViewer.content (
-                                         TextBlock.create [
-    //                                        TextBlock.background "Gray"
-                                            TextBlock.horizontalScrollBarVisibility ScrollBarVisibility.Disabled
-                                            TextBlock.verticalScrollBarVisibility ScrollBarVisibility.Disabled
-                                            TextBlock.text model.SelectedConnectionRecieved
-                                        ]
-                                    )
-                                ]
-                             )
-                        ]
-                        GridSplitter.create [
-                            Grid.row 1
-                        ]
-                        ScrollViewer.create [
-                            ScrollViewer.horizontalScrollBarVisibility ScrollBarVisibility.Visible
-                            ScrollViewer.verticalScrollBarVisibility ScrollBarVisibility.Visible
-                            Grid.row 2
-                            ScrollViewer.content (
-                                TextBlock.create [
-                                    TextBlock.horizontalScrollBarVisibility ScrollBarVisibility.Disabled
-                                    TextBlock.verticalScrollBarVisibility ScrollBarVisibility.Disabled
-                                    TextBlock.text model.SelectedConnectionSent
-                                ]
-                            )
-                        ]
-                    ]
-                ]
+                RequestReplyView.view model.SelectedConnectionRecieved model.SelectedConnectionSent 
              ]
         ]
