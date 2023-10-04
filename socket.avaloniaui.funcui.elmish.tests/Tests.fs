58ad2b9928e1a87e4a8c2d05b79369dc1466e483
bjorn-einar-bjartnes-4ss <bjartnes@gmail.com>
2023-10-04 07:28:19 +0200

    Update Tests.fs

--- socket.avaloniaui.funcui.elmish.tests/Tests.fs
+++ socket.avaloniaui.funcui.elmish.tests/Tests.fs
@@ -16,5 +16,23 @@ let ``My test`` () =
         TextToSend = ""
     }
 
-    let initView = SocketViews.mainView model (fun (x:Msg) -> ())
+    let initView = SocketViews.mainView model (fun (_:Msg) -> ())
     Assert.NotEmpty(initView.Attrs)
+
+
+[<Fact>]
+let ``My test 1`` () =
+    let model = {
+        Connections = [] 
+        SelectedItem = None 
+        SelectedConnectionSent = ""
+        SelectedConnectionRecieved = ""
+        TextToSend = ""
+    }
+
+    let msg = ChangeTextToSend  "HTTP/1.1 200 OK"
+
+    let (model', msg')= Update.update msg model
+
+    Assert.Equal("HTTP/1.1 200 OK", model'.TextToSend)
+
